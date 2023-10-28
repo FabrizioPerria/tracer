09534d15a8b2652ab1734c85539f08f3a0136801
Fabrizio Perria <fabrizio.perria@gmail.com>
2023-10-28 16:34:39 +0200

    Fix type bug

--- tracer.hpp
+++ tracer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <_types/_uint64_t.h>
+#include <any>
 #include <atomic>
 #include <ctime>
 #include <fstream>
@@ -16,54 +17,43 @@
 #define TRACER_BEGIN(c, n) TRACER.durationBegin(c, n)
 #define TRACER_END(c, n) TRACER.durationEnd(c, n)
 #define TRACER_SCOPE(c, n) ScopedTrace ____scope(c, n)
-#define TRACER_BEGIN_INT(c, n, k, v) TRACER.durationBegin(c, n, ArgType::INT, k, ( void * ) v)
-#define TRACER_END_INT(c, n, k, v) TRACER.durationEnd(c, n, ArgType::INT, k, ( void * ) v)
-#define TRACER_SCOPE_INT(c, n, k, v) ScopedTrace ____scope(c, n, ArgType::INT, k, ( void * ) v)
-#define TRACER_BEGIN_STR(c, n, k, v) TRACER.durationBegin(c, n, ArgType::STRING, k, ( void * ) v)
-#define TRACER_END_STR(c, n, k, v) TRACER.durationEnd(c, n, ArgType::STRING, k, ( void * ) v)
-#define TRACER_SCOPE_STR(c, n, k, v) ScopedTrace ____scope(c, n, ArgType::STRING, k, ( void * ) v)
 
-#define TRACER_ASYNC_START(c, n, id) TRACER.asyncBegin(c, n, ( void * ) (id))
-#define TRACER_ASYNC_STEP(c, n, id, step) TRACER.asyncStep(c, n, ( void * ) (id), ( void * ) (step))
-#define TRACER_ASYNC_FINISH(c, n, id) TRACER.asyncEnd(c, n, ( void * ) (id))
-#define TRACER_ASYNC_START_INT(c, n, id, k, v) TRACER.asyncBegin(c, n, ( void * ) (id), ArgType::INT, k, ( void * ) v)
-#define TRACER_ASYNC_STEP_INT(c, n, id, step, k, v)                                                                    \
-	TRACER.asyncStep(c, n, ( void * ) (id), ( void * ) (step), ArgType::INT, k, ( void * ) v)
-#define TRACER_ASYNC_FINISH_INT(c, n, id, k, v) TRACER.asyncEnd(c, n, ( void * ) (id), ArgType::INT, k, ( void * ) v)
-#define TRACER_ASYNC_START_STR(c, n, id, k, v)                                                                         \
-	TRACER.asyncBegin(c, n, ( void * ) (id), ArgType::STRING, k, ( void * ) v)
-#define TRACER_ASYNC_STEP_STR(c, n, id, step, k, v)                                                                    \
-	TRACER.asyncStep(c, n, ( void * ) (id), ( void * ) (step), ArgType::STRING, k, ( void * ) v)
-#define TRACER_ASYNC_FINISH_STR(c, n, id, k, v) TRACER.asyncEnd(c, n, ( void * ) (id), ArgType::STRING, k, ( void * ) v)
+#define TRACER_BEGIN_INT(c, n, k, v) TRACER.durationBegin<int>(c, n, k, v)
+#define TRACER_END_INT(c, n, k, v) TRACER.durationEnd<int>(c, n, k, v)
+#define TRACER_SCOPE_INT(c, n, k, v) ScopedTrace<int> ____scope(c, n, k, v)
 
-#define TRACER_FLOW_START(c, n, id) TRACER.flowStart(c, n, ( void * ) (id))
-#define TRACER_FLOW_STEP(c, n, id, step) TRACER.flowStep(c, n, ( void * ) (id), ( void * ) (step))
-#define TRACER_FLOW_FINISH(c, n, id) TRACER.flowFinish(c, n, ( void * ) (id))
-#define TRACER_FLOW_START_INT(c, n, id, k, v) TRACER.flowStart(c, n, ( void * ) (id), ArgType::INT, k, ( void * ) v)
-#define TRACER_FLOW_STEP_INT(c, n, id, step, k, v)                                                                     \
-	TRACER.flowStep(c, n, ( void * ) (id), ( void * ) (step), ArgType::INT, k, ( void * ) v)
-#define TRACER_FLOW_FINISH_INT(c, n, id, k, v) TRACER.flowFinish(c, n, ( void * ) (id), ArgType::INT, k, ( void * ) v)
-#define TRACER_FLOW_START_STR(c, n, id, k, v) TRACER.flowStart(c, n, ( void * ) (id), ArgType::STRING, k, ( void * ) v)
-#define TRACER_FLOW_STEP_STR(c, n, id, step, k, v)                                                                     \
-	TRACER.flowStep(c, n, ( void * ) (id), ( void * ) (step), ArgType::STRING, k, ( void * ) v)
-#define TRACER_FLOW_FINISH_STR(c, n, id, k, v)                                                                         \
-	TRACER.flowFinish(c, n, ( void * ) (id), ArgType::STRING, k, ( void * ) v)
+#define TRACER_BEGIN_STR(c, n, k, v) TRACER.durationBegin<std::string>(c, n, k, v)
+#define TRACER_END_STR(c, n, k, v) TRACER.durationEnd<std::string>(c, n, k, v)
+#define TRACER_SCOPE_STR(c, n, k, v) ScopedTrace<std::string> ____scope(c, n, k, v)
+
+#define TRACER_ASYNC_START(c, n, id) TRACER.asyncBegin(c, n, id)
+#define TRACER_ASYNC_STEP(c, n, id, step) TRACER.asyncStep(c, n, id, step)
+#define TRACER_ASYNC_FINISH(c, n, id) TRACER.asyncEnd(c, n, id)
+
+#define TRACER_ASYNC_START_INT(c, n, id, k, v) TRACER.asyncBegin<int>(c, n, id, k, v)
+#define TRACER_ASYNC_FINISH_INT(c, n, id, k, v) TRACER.asyncEnd<int>(c, n, id, k, v)
+
+#define TRACER_ASYNC_START_STR(c, n, id, k, v) TRACER.asyncBegin<std::string>(c, n, id, k, v)
+#define TRACER_ASYNC_FINISH_STR(c, n, id, k, v) TRACER.asyncEnd<std::string>(c, n, id, k, v)
+
+#define TRACER_FLOW_START(c, n, id) TRACER.flowStart(c, n, id)
+#define TRACER_FLOW_STEP(c, n, id, step) TRACER.flowStep(c, n, id, step)
+#define TRACER_FLOW_FINISH(c, n, id) TRACER.flowFinish(c, n, id)
+
+#define TRACER_FLOW_START_INT(c, n, id, k, v) TRACER.flowStart<int>(c, n, id, k, v)
+#define TRACER_FLOW_FINISH_INT(c, n, id, k, v) TRACER.flowFinish<int>(c, n, id, k, v)
+
+#define TRACER_FLOW_START_STR(c, n, id, k, v) TRACER.flowStart<std::string>(c, n, id, k, v)
+#define TRACER_FLOW_FINISH_STR(c, n, id, k, v) TRACER.flowFinish<std::string>(c, n, id, k, v)
 
 #define TRACER_INSTANT(c, n) TRACER.instant(c, n)
 
 #define TRACER_COUNTER(c, n, v) TRACER.counter(c, n, v)
 
-#define TRACER_META_PROCESS_NAME(n) TRACER.metadata("process_name", ( void * ) n)
-#define TRACER_META_THREAD_NAME(n) TRACER.metadata("thread_name", ( void * ) n)
+#define TRACER_META_PROCESS_NAME(n) TRACER.metadata("process_name", n)
+#define TRACER_META_THREAD_NAME(n) TRACER.metadata("thread_name", n)
 
-typedef enum
-{
-	NONE,
-	INT,
-	STRING,
-} ArgType;
-
-typedef struct event_t
+template<typename T = void *> struct Event
 {
 	const std::string name;
 	const std::string category;
@@ -72,10 +62,9 @@ typedef struct event_t
 	uint32_t pid;
 	uint32_t tid;
 	char phase;
-	ArgType argType;
 	const std::string argName;
-	void *argValue;
-} Event;
+	std::variant<int, std::string, void *> argValue;
+};
 
 class TracerManager
 {
@@ -93,61 +82,77 @@ class TracerManager
 		isTracing = true;
 	}
 
-	inline void durationBegin(const std::string &category, const std::string &name, ArgType type = ArgType::NONE,
-							  const std::string &key = "", void *value = nullptr)
+	template<typename T = void *>
+	inline void durationBegin(
+		const std::string &category, const std::string &name, const std::string &key = "", T value = nullptr
+	)
 	{
-		processEvent(category, name, 'B', 0, type, key, value);
+		processEvent<T>(category, name, 'B', 0, key, value);
 	}
 
-	inline void durationEnd(const std::string &category, const std::string &name, ArgType type = ArgType::NONE,
-							const std::string &key = "", void *value = nullptr)
+	template<typename T = void *>
+	inline void durationEnd(
+		const std::string &category, const std::string &name, const std::string &key = "", T value = nullptr
+	)
 	{
-		processEvent(category, name, 'E', 0, type, key, value);
+		processEvent<T>(category, name, 'E', 0, key, value);
 	}
 
 	inline void instant(const std::string &category, const std::string &name)
 	{
-		processEvent(category, name, 'i');
+		processEvent<void *>(category, name, 'i');
 	}
 
 	inline void counter(const std::string &category, const std::string &name, int64_t count)
 	{
-		processEvent(category, name, 'C', 0, ArgType::INT, name, ( void * ) count);
+		processEvent<int>(category, name, 'C', 0, name, count);
 	}
 
-	inline void asyncBegin(const std::string &category, const std::string &name, void *id)
+	template<typename T = void *>
+	inline void asyncBegin(
+		const std::string &category, const std::string &name, void *id, const std::string &key = "", T value = nullptr
+	)
 	{
-		processEvent(category, name, 'S', id);
+		processEvent<T>(category, name, 'S', id, key, value);
 	}
 
-	inline void asyncStep(const std::string &category, const std::string &name, void *id, void *step)
+	inline void asyncStep(const std::string &category, const std::string &name, void *id, const std::string &step)
 	{
-		processEvent(category, name, 'T', id, ArgType::STRING, "step", step);
+		processEvent<std::string>(category, name, 'T', id, "step", step);
 	}
 
-	inline void asyncEnd(const std::string &category, const std::string &name, void *id)
+	template<typename T = void *>
+	inline void asyncEnd(
+		const std::string &category, const std::string &name, void *id, const std::string &key = "", T value = nullptr
+	)
 	{
-		processEvent(category, name, 'F', id);
+		processEvent<T>(category, name, 'F', id, key, value);
 	}
 
-	inline void flowStart(const std::string &category, const std::string &name, void *id)
+	template<typename T = void *>
+	inline void flowStart(
+		const std::string &category, const std::string &name, void *id, const std::string &key = "", T value = nullptr
+	)
 	{
-		processEvent(category, name, 's', id);
+		processEvent<T>(category, name, 's', id, key, value);
 	}
 
-	inline void flowStep(const std::string &category, const std::string &name, void *id, void *step)
+	inline void flowStep(const std::string &category, const std::string &name, void *id, const std::string &step)
 	{
-		processEvent(category, name, 't', id, ArgType::STRING, "step", step);
+		processEvent<std::string>(category, name, 't', id, "step", step);
 	}
 
-	inline void flowFinish(const std::string &category, const std::string &name, void *id)
+	template<typename T = void *>
+	inline void flowFinish(
+		const std::string &category, const std::string &name, void *id, const std::string &key = "", T value = nullptr
+	)
 	{
-		processEvent(category, name, 'f', id);
+		processEvent<T>(category, name, 'f', id, key, value);
 	}
 
-	inline void metadata(const std::string &metadataName, void *argValue)
+	inline void metadata(const std::string &metadataName, const std::string &argValue)
 	{
-		processEvent("", metadataName, 'M', 0, ArgType::STRING, "name", argValue);
+		processEvent<std::string>("", metadataName, 'M', 0, "name", argValue);
 	}
 
 	void flush(void)
@@ -172,29 +177,22 @@ class TracerManager
 			traceBuffer += "\"ts\":" + std::to_string(raw.ts - timeOffset) + ",";
 			traceBuffer += "\"ph\":\"" + std::string(1, raw.phase) + "\",";
 			traceBuffer += "\"name\":\"" + raw.name + "\",";
-			traceBuffer += "\"args\":";
+			traceBuffer += "\"id\":\"" + getIdStr(raw.id) + "\"";
 
-			switch (raw.argType)
-			{
-			case INT:
-				traceBuffer += "{\"" + raw.argName + "\":" + std::to_string(( int64_t ) raw.argValue) + "}";
-				break;
-			case STRING:
-				traceBuffer += "{\"" + raw.argName + "\":\"" + std::string((const char * ) raw.argValue) + "\"}";
-				break;
-			case NONE:
-			default:
-				traceBuffer += "{}";
-				break;
-			}
-
-			if (raw.id)
-			{
-				std::stringstream ss;
-				ss << ",\"id\":\"" << std::hex << std::setw(8) << std::setfill('0')
-				   << static_cast<uint32_t>(reinterpret_cast<uintptr_t>(raw.id)) << "\"" << std::dec;
-				traceBuffer += ss.str();
-			}
+			std::visit(
+				[&traceBuffer, &raw](auto &&argValue) {
+					using T = std::decay_t<decltype(argValue)>;
+					if constexpr (std::is_same_v<T, int>)
+					{
+						traceBuffer += ",\"args\": {\"" + raw.argName + "\":" + std::to_string(argValue) + "}";
+					}
+					else if constexpr (std::is_same_v<T, std::string>)
+					{
+						traceBuffer += ",\"args\": {\"" + raw.argName + "\":\"" + argValue + "\"}";
+					}
+				},
+				raw.argValue
+			);
 
 			traceBuffer += "}";
 			trace << traceBuffer;
@@ -222,8 +220,8 @@ class TracerManager
 	static std::once_flag createInstance;
 	std::once_flag firstEntry;
 
-	std::vector<Event> eventBuffer;
-	std::vector<Event> flushBuffer;
+	std::vector<Event<std::variant<int, std::string>>> eventBuffer;
+	std::vector<Event<std::variant<int, std::string>>> flushBuffer;
 
 	std::atomic<uint16_t> eventsInProgress;
 
@@ -237,19 +235,28 @@ class TracerManager
 
 	uint64_t timeOffset;
 
-	double getTime()
+	uint64_t getTime()
 	{
 		return std::chrono::duration_cast<std::chrono::microseconds>(
-				   std::chrono::high_resolution_clock::now().time_since_epoch())
+				   std::chrono::high_resolution_clock::now().time_since_epoch()
+		)
 			.count();
 	}
 
-	void processEvent(const std::string &category, const std::string &name, char ph, void *id = nullptr,
-					  ArgType arg_type = NONE, const std::string &arg_name = "", void *arg_value = nullptr)
+	template<typename T = void *>
+	void processEvent(
+		const std::string &category, const std::string &name, char ph, void *id = 0, const std::string &arg_name = "",
+		T arg_value = nullptr
+	)
 	{
 		std::atomic_fetch_add(&eventsInProgress, 1);
-		Event ev{name,	   category, id,	   ( uint64_t ) getTime(), ( uint32_t ) getpid(), gettid(), ph,
-				 arg_type, arg_name, arg_value};
+		Event<std::variant<int, std::string>> ev{name,	   category, id,	   getTime(), (uint32_t)getpid(),
+												 gettid(), ph,		 arg_name, nullptr};
+
+		if constexpr (std::is_same_v<T, int> || std::is_same_v<T, std::string>)
+		{
+			ev.argValue = arg_value;
+		}
 
 		std::lock_guard<std::mutex> lock(eventMutex);
 		eventBuffer.push_back(ev);
@@ -264,30 +271,40 @@ class TracerManager
 	{
 		return std::hash<std::thread::id>()(std::this_thread::get_id());
 	}
+
+	std::string getIdStr(void *id)
+	{
+		if (id)
+		{
+			std::stringstream ss;
+			ss << "0x" << std::hex << reinterpret_cast<std::uintptr_t>(id);
+			return ss.str();
+		}
+		else
+			return "0x0";
+	}
 };
 
 std::unique_ptr<TracerManager> TracerManager::instance;
 std::once_flag TracerManager::createInstance;
 
-class ScopedTrace
+template<typename T = void *> class ScopedTrace
 {
   public:
-	ScopedTrace(const std::string &category, const std::string &name, ArgType type = ArgType::NONE,
-				std::string key = "", void *value = nullptr)
-		: category(category), name(name), type(type), key(key), value(value)
+	ScopedTrace(const std::string &category, const std::string &name, std::string key = "", T value = nullptr)
+		: category(category), name(name), key(key), value(value)
 	{
-		TRACER.durationBegin(category, name, type, key, value);
+		TRACER.durationBegin(category, name, key, value);
 	}
 
 	~ScopedTrace()
 	{
-		TRACER.durationEnd(category, name, type, key, value);
+		TRACER.durationEnd(category, name, key, value);
 	}
 
   private:
 	std::string category;
 	std::string name;
-	ArgType type;
 	std::string key;
-	void *value;
+	T value;
 };
